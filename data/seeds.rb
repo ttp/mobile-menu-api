db9a578c832ac3b817e430cd6fa1b8ba642a9c20
Taras Taday <human.ttp@gmail.com>
2012-12-15 16:29:59 +0200

    generate points for rivne

--- data/seeds.rb
+++ data/seeds.rb
@@ -9,9 +9,13 @@ db       = client['mobilemenu_test']
 accounts = db['accounts']
 places   = db['places']
 
+puts "clearing accounts..."
 accounts.remove
+
+puts "clearing places..."
 places.remove
 
+# Ukraine
 # left    48.283193,22.159424
 # right   48.698212,39.666138
 # top     52.001792,33.266602
@@ -23,19 +27,32 @@ lat_max = 52.001792
 lng_min = 22.159424
 lng_max = 39.666138
 
+
+# Rivne
+# left    50.622732,26.202908
+# right   50.615162,26.287365
+# top     50.652508,26.235609
+# bottom  50.594191,26.233892
+
+lat_min = 50.594191
+lat_max = 50.652508
+
+lng_min = 26.202908
+lng_max = 26.287365
+
 types = ['bar', 'cafeteria', 'coffee', 'fastfood', 'pizzaria', 'restaurant']
 
-
+puts "creating data..."
 10.times do |i|
   account_id = accounts.insert({"name" => "test"})
   
-  10000.times do |i|
+  50.times do |j|
     lat = rand(lat_min..lat_max).round(6)
     lng = rand(lng_min..lng_max).round(6)
     place = {
       "account_id" => account_id,
-      "name" => "palce #{i}",
-      "description" => "description #{i}",
+      "name" => "palce #{i} #{j}",
+      "description" => "description #{i} #{j}",
 
       "place_type_code" => types.sample,
       "show_on_map" => true,
@@ -48,7 +65,7 @@ types = ['bar', 'cafeteria', 'coffee', 'fastfood', 'pizzaria', 'restaurant']
     }
 
     places.insert(place)
-    print "." if i % 100 == 0
+    print "." if j % 20 == 0
   end
   puts ""
 end
